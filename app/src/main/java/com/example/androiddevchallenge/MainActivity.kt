03d4c44a2f049015ed25ca8f27cae124751e6343
zhangying <zy@layouzhibo.com>
2021-03-04 20:45:48 +0800

    time count down finish

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -18,14 +18,31 @@ package com.example.androiddevchallenge
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.animation.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.lifecycle.MutableLiveData
 import com.example.androiddevchallenge.ui.theme.MyTheme
+import rx.Observable
+import rx.Subscription
+import rx.android.schedulers.AndroidSchedulers
+import rx.schedulers.Schedulers
+import java.util.*
+import java.util.concurrent.TimeUnit
 
+@ExperimentalAnimationApi
 class MainActivity : AppCompatActivity() {
+
+    private val mCountDownData = MutableLiveData(0L)
+    private var mTimer: Subscription? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -33,29 +50,57 @@ class MainActivity : AppCompatActivity() {
                 MyApp()
             }
         }
+        val instance = Calendar.getInstance()
+        instance.set(2021, 2, 9, 23, 59, 59)
+        val endMills = instance.timeInMillis
+
+        mTimer = Observable.interval(0, 1, TimeUnit.SECONDS)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe {
+                val diff = endMills - System.currentTimeMillis()
+                mCountDownData.value = diff
+            }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (mTimer?.isUnsubscribed == false) {
+            mTimer?.unsubscribe()
+        }
+    }
+
+    @Composable
+    fun MyApp() {
+        Surface(color = MaterialTheme.colors.background, modifier = Modifier.fillMaxSize()) {
+            Column {
+                var diff = mCountDownData.observeAsState().value ?: 0L
+                val day = diff / 60 / 60 / 24 / 1000
+                diff -= day * 60 * 60 * 24 * 1000
+                val hour = diff / 60 / 60 / 1000
+                diff -= hour * 60 * 60 * 1000
+                val min = diff / 60 / 1000
+                diff -= min * 60 * 1000
+                val second = diff / 1000
+                Text(text = "距离比赛结束还有: ${day}天 ${hour}小时 ${min}分 ${second}秒")
+            }
+        }
     }
 }
 
-// Start building your app here!
-@Composable
-fun MyApp() {
-    Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
-    }
-}
-
-@Preview("Light Theme", widthDp = 360, heightDp = 640)
-@Composable
-fun LightPreview() {
-    MyTheme {
-        MyApp()
-    }
-}
-
-@Preview("Dark Theme", widthDp = 360, heightDp = 640)
-@Composable
-fun DarkPreview() {
-    MyTheme(darkTheme = true) {
-        MyApp()
-    }
-}
+//
+//@Preview("Light Theme", widthDp = 360, heightDp = 640)
+//@Composable
+//fun LightPreview() {
+//    MyTheme {
+//        MyApp()
+//    }
+//}
+//
+//@Preview("Dark Theme", widthDp = 360, heightDp = 640)
+//@Composable
+//fun DarkPreview() {
+//    MyTheme(darkTheme = true) {
+//        MyApp()
+//    }
+//}
