5f02e692e0592ab82edf472d258e24b4b76cedd3
zhangying <zy@layouzhibo.com>
2021-03-09 19:08:58 +0800

    process spotless

--- app/src/main/java/com/example/androiddevchallenge/timer/ui/CountDownTimer.kt
+++ app/src/main/java/com/example/androiddevchallenge/timer/ui/CountDownTimer.kt
@@ -1,20 +1,32 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge.timer.ui
 
 import android.util.Log
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.Button
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
@@ -24,8 +36,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.drawscope.DrawStyle
-import androidx.compose.ui.graphics.drawscope.Fill
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.drawscope.rotate
 import androidx.compose.ui.unit.dp
@@ -58,7 +68,6 @@ fun CountDownTimer(viewModel: CountDownViewModel) {
                     this.drawCircle(Color.Black, radius = 10f)
                 }
             )
-
         } else {
             var editedCountDownSeconds by remember(calculation = { mutableStateOf(20) })
             OutlinedTextField(
@@ -77,9 +86,11 @@ fun CountDownTimer(viewModel: CountDownViewModel) {
 
             Spacer(modifier = Modifier.height(50.dp))
 
-            Button(onClick = {
-                viewModel.startCountDownTimer(editedCountDownSeconds)
-            }) {
+            Button(
+                onClick = {
+                    viewModel.startCountDownTimer(editedCountDownSeconds)
+                }
+            ) {
                 Text(text = "OK")
             }
         }
